e0d6f922d4c56b7957dfa9dd0f3dfd1daeafadc1
Kitamura-Philip <57215560+Kitamura-Philip@users.noreply.github.com>
2022-08-31 10:32:20 +0300

    Update __init__.py

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -90,7 +90,19 @@ def create_app(test_config=None):
     TEST: When you click the trash icon next to a question, the question will be removed.
     This removal will persist in the database and when you refresh the page.
     """
+    @app.route('/questions/<int:id>', methods=['DELETE'])
+    def delete_question(id):
+        if request.method == "DELETE":
+            q = Question.query.filter_by(id=id).one_or_none()
+            if q is None:
+                abort(404)
 
+            q.delete()
+            return jsonify({
+                'deleted': id,
+                'success': True,
+                'total_questions': len(Question.query.all())
+            })
     """
     @TODO:
     Create an endpoint to POST a new question,
@@ -112,7 +124,43 @@ def create_app(test_config=None):
     only question that include that string within their question.
     Try using the word "title" to start.
     """
+    @app.route('/questions', methods=['POST'])
+    def create_a_question():
+        if request.method == "POST":
+            body = request.get_json()
+            question = body.get('question', None)
+            answer = body.get('answer', None)
+            difficulty = body.get('difficulty', None)
+            category = body.get('category',  None)
+            
+            searchTerm = body.get('searchTerm', None)
+            try:
+                if searchTerm:
+                    questions = Question.query.filter(Question.question.ilike(f"%{searchTerm}%")).all()
+                    paginated_questions = paginate_questions(request, questions)
+                    
+                    return jsonify({
+                        'success': True,
+                        'questions': paginated_questions,
+                        'total_questions': len(paginated_questions)
+                    })
 
+                else:
+                    q = Question(question=question, answer=answer, difficulty=difficulty, category=category)
+                    q.insert()
+
+                    questions = Question.query.order_by(Question.id).all()
+                    paginated_questions = paginate_questions(request, questions)
+
+                    return jsonify({
+                        'success': True,
+                        'question_created': q.question,
+                        'created': q.id,
+                        'questions': paginated_questions,
+                        'total_questions': len(Question.query.all())
+                    })
+            except:
+                abort(422)
     """
     @TODO:
     Create a GET endpoint to get questions based on category.
@@ -121,7 +169,25 @@ def create_app(test_config=None):
     categories in the left column will cause only questions of that
     category to be shown.
     """
+    @app.route('/categories/<int:category_id>/questions', methods=['GET'])
+    def get_questions_by_category(category_id):
+        if request.method == "GET":
+            category = Category.query.filter_by(id=category_id).one_or_none()
+            if category is None:
+                abort(404)
+            try:
+                questions = Question.query.filter_by(category=category.id).all()
+                paginated_questions = paginate_questions(request, questions)
 
+                return jsonify({
+                    'success': True,
+                    'total_questions': len(Question.query.all()),
+                    'current_category': category.type,
+                    'questions': paginated_questions
+                })
+
+            except:
+                abort(400)
     """
     @TODO:
     Create a POST endpoint to get questions to play the quiz.
@@ -133,11 +199,74 @@ def create_app(test_config=None):
     one question at a time is displayed, the user is allowed to answer
     and shown whether they were correct or not.
     """
+    @app.route('/play', methods=['POST'])
+    def play_game():
+        if request.method == "POST":
+            try:
+                body = request.get_json()
+                prev_questions = body.get('previous_questions', None)
+                category = body.get('quiz_category', None)
 
+                cat_id = category['id']
+                next_question = None
+                
+                if cat_id != 0:
+                    av_questions = Question.query.filter_by(category=cat_id).filter(Question.id.notin_((prev_questions))).all()    
+                else:
+                    av_questions = Question.query.filter(Question.id.notin_((prev_questions))).all()
+                
+                if len(av_questions) > 0:
+                    next_question = random.choice(av_questions).format()
+                
+                return jsonify({
+                    'question': next_question,
+                    'success': True,
+                })
+            except:
+                abort(422)
     """
     @TODO:
     Create error handlers for all expected errors
     including 404 and 422.
     """
+    @app.errorhandler(400)
+    def bad_request_error(error):
+        return jsonify({
+            "success": False,
+            "error": 400,
+            "message": "bad request"
+        }), 400
+
+    @app.errorhandler(404)
+    def not_found(error):
+        return jsonify({
+            "success": False,
+            "error": 404,
+            "message": "resource not found"
+        }), 404
+    
+    @app.errorhandler(405)
+    def method_not_allowed(error):
+        return jsonify({
+            'success': False,
+            'error': 405,
+            'message': 'method not allowed'
+        }), 405
+
+    @app.errorhandler(422)
+    def unprocessable(error):
+        return jsonify({
+            "success": False,
+            "error": 422,
+            "message": "unprocessable"
+        }), 422
+
+    @app.errorhandler(500)
+    def internal_server_error(error):
+        return jsonify({
+            "success": False,
+            "error": 500,
+            "message": "internal server error"
+        }), 422
 
     return app
